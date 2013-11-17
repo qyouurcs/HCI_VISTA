ddfcc092a02ab03cde224ac69a7ce1d3ecf4e015
ltran <ltran>
2013-11-17 13:03:37 -0800

    add opencv naive files for android

---
+++ HCI_Vista_Low/jni/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+include ../../sdk/native/jni/OpenCV.mk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+
+LOCAL_MODULE    := native_activity
+LOCAL_SRC_FILES := native.cpp
+LOCAL_LDLIBS    += -lm -llog -landroid
+LOCAL_STATIC_LIBRARIES += android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
