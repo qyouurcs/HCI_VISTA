ee009f8acaf879b96f59af503b26f05e84093276
qyouurcs <qyou@cs.rochester.edu>
2013-11-26 00:59:48 -0500

    Add voice recognition in the app, update the view to make it looks like an real view controller.

--- HCI_Vista_Low/src/com/example/hci_vista_low/DisplayResults.java
+++ HCI_Vista_Low/src/com/example/hci_vista_low/DisplayResults.java
@@ -1,87 +1,198 @@
 package com.example.hci_vista_low;
 
+import java.util.ArrayList;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-
-
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MotionEvent;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.example.hci_vista_low.Selector;
-import com.example.hci_vista_low.ListOfSelectors;
-
-
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class DisplayResults extends Activity {
-	Mat mat;              // OpenCV matrix for image
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 9999;
+	Mat mat; // OpenCV matrix for image
 	Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
 	ImageView result;
-	private static final String    TAG                 = "OCVSample::Activity";
-	  
+	private ProgressBar pb;
+	private static final String TAG = "OCVSample::Activity";
+	private SingleTouchEventView singleTouch = null;
+
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-		  @Override
-		  public void onManagerConnected(int status) {
-		      switch (status) {
-		          case LoaderCallbackInterface.SUCCESS:
-		          {
-		              Log.i(TAG, "OpenCV loaded successfully");
-		              OpenCVInit();
-		              display();
-		          } break;
-		          default:
-		          {
-		              super.onManagerConnected(status);
-		          } break;
-		      }
-		  }  
-	  };
-		  
-	   
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+				OpenCVInit();
+				display();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.display_results, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+
+		case R.id.action_compose:
+			speak();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void speak() {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		// Specify the calling package to identify your application
+		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+				.getPackage().getName());
+
+		// Given an hint to the recognizer about what the user is going to say
+		intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+
+		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+			// If Voice recognition is successful then it returns RESULT_OK
+			if (resultCode == RESULT_OK) {
+
+				ArrayList<String> textMatchList = data
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				if (!textMatchList.isEmpty()) {
+					// If first Match contains the 'search' word
+					// Then start web search.
+					if (textMatchList.get(0).contains("search")) {
+
+						String searchQuery = textMatchList.get(0).replace(
+								"search", " ");
+						Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+						search.putExtra(SearchManager.QUERY, searchQuery);
+						startActivity(search);
+					} else {
+						// populate the Matches
+						String msg = textMatchList.get(0);
+						pb.setVisibility(View.VISIBLE);
+						AsyncItemDec dec = new AsyncItemDec();
+						dec.execute(100);
+						// showToastMessage(msg.trim());
+					}
+
+				}
+				// Result code for various error.
+			} else if (resultCode == RecognizerIntent.RESULT_AUDIO_ERROR) {
+				showToastMessage("Audio Error");
+			} else if (resultCode == RecognizerIntent.RESULT_CLIENT_ERROR) {
+				showToastMessage("Client Error");
+			} else if (resultCode == RecognizerIntent.RESULT_NETWORK_ERROR) {
+				showToastMessage("Network Error");
+			} else if (resultCode == RecognizerIntent.RESULT_NO_MATCH) {
+				showToastMessage("No Match");
+			} else if (resultCode == RecognizerIntent.RESULT_SERVER_ERROR) {
+				showToastMessage("Server Error");
+			}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	void showToastMessage(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		Log.i(TAG, "Trying to load OpenCV library");
-        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback)){
-        	Log.e(TAG, "Cannot connect to OpenCV Manager");
-        }
-        else{ 
-        	Log.i(TAG, "opencv successfull"); 
-        }
-	}
-	
-    public void OpenCVInit()
-    {
-        setContentView(R.layout.activity_display_results);
-		mat = new Mat();
-    }
 
-    public void display()
-    {
+		Log.i(TAG, "Trying to load OpenCV library");
+		if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this,
+				mLoaderCallback)) {
+			Log.e(TAG, "Cannot connect to OpenCV Manager");
+		} else {
+			Log.i(TAG, "opencv successfull");
+		}
+
+	}
+
+	class AsyncItemDec extends AsyncTask<Integer, Integer, String> {
+		protected String doInBackground(Integer... params) {
+
+			for (int i = 0; i <= 100; i++) {
+				pb.setProgress(i);
+				publishProgress(i, 5);
+				try {
+					Thread.sleep(params[0]);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return "";
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+		}
+
+		protected void onPostExecute(String result) {
+			// setTitle(result);
+			pb.setVisibility(View.INVISIBLE);
+		}
+
+	}
+
+	public void OpenCVInit() {
+		setContentView(R.layout.activity_display_results);
+		pb = (ProgressBar) findViewById(R.id.item_dec_progress);
+		singleTouch = (SingleTouchEventView) findViewById(R.id.single_touch);
+		// result = (ImageView) findViewById(R.id.result_pic);
+		mat = new Mat();
+	}
+
+	public void display() {
 		Intent intent = getIntent();
-		String result_fn = intent.getStringExtra(MainActivity.EXTRA_INTENT_MESSAGE);
-		//Bitmap image = BitmapFactory.decodeFile(result_fn); 
+		String result_fn = intent
+				.getStringExtra(MainActivity.EXTRA_INTENT_MESSAGE);
+		// Bitmap image = BitmapFactory.decodeFile(result_fn);
 		// result = (ImageView) findViewById(R.id.result_pic);
 		// result.setImageBitmap(image);
-		setContentView(new SingleTouchEventView(this, null,result_fn ));
-    }	
+		// result = new SingleTouchEventView(this, null, result_fn);
+		// setContentView(R.layout.activity_display_results);
+		// setContentView(new SingleTouchEventView(this, null, result_fn));
+		singleTouch.setImageFile(result_fn);
+		// addContentView(new SingleTouchEventView(this, null, result_fn));
+	}
 }
--- HCI_Vista_Low/src/com/example/hci_vista_low/SingleTouchEventView.java
+++ HCI_Vista_Low/src/com/example/hci_vista_low/SingleTouchEventView.java
@@ -18,55 +18,75 @@ import android.widget.ImageView;
 import com.example.hci_vista_low.ListOfSelectors;
 
 public class SingleTouchEventView extends ImageView {
-  private Paint paint = new Paint();
-  private ListOfSelectors sels = new ListOfSelectors();
-  private static final String    TAG                 = "LAM";
-  Bitmap image;
-  
-  public SingleTouchEventView(Context context, AttributeSet attrs, String fn) {
-    super(context, attrs);
-    paint.setAntiAlias(true);
-    paint.setStrokeWidth(6f);
-    paint.setColor(Color.BLACK);
-    paint.setStyle(Paint.Style.STROKE);
-    paint.setStrokeJoin(Paint.Join.ROUND);
-	image = BitmapFactory.decodeFile(fn); 
-	this.setImageBitmap(image);
-  }
+	private Paint paint = new Paint();
+	private ListOfSelectors sels = new ListOfSelectors();
+	private static final String TAG = "LAM";
+	Bitmap image = null;
 
-  @Override
-  protected void onDraw(Canvas canvas) {
-	  Rect dest = new Rect(0, 0, getWidth(), getHeight());
-	  canvas.drawBitmap(image, null, dest, paint);
-      sels.drawSelectors(canvas, paint);
-  }
+	public SingleTouchEventView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		paint.setAntiAlias(true);
+		paint.setStrokeWidth(6f);
+		paint.setColor(Color.BLACK);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+	}
+	
+	public SingleTouchEventView(Context context, AttributeSet attrs, String fn) {
+		super(context, attrs);
+		paint.setAntiAlias(true);
+		paint.setStrokeWidth(6f);
+		paint.setColor(Color.BLACK);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		image = BitmapFactory.decodeFile(fn);
+		this.setImageBitmap(image);
+	}
+	
+	public void setImageFile(String fn)
+	{
+		if(image != null)
+			image.recycle();//first release the memory.
+		image = BitmapFactory.decodeFile(fn);
+		this.setImageBitmap(image);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		Rect dest = new Rect(0, 0, getWidth(), getHeight());
+		if( image != null)
+		{
+			canvas.drawBitmap(image, null, dest, paint);
+			sels.drawSelectors(canvas, paint);
+		}
+	}
 
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    float eventX = event.getX();
-    float eventY = event.getY();
-    
-    switch (event.getAction()) {
-    case MotionEvent.ACTION_MOVE:
-    	Log.i(TAG, "MOVE Coordinates "+ event.getX() + " "+ event.getY());
-    	sels.movingPoint(new Point((int)eventX, (int)eventY));
-    	break;
-    case MotionEvent.ACTION_POINTER_DOWN:
-        Log.i(TAG, "APD Coordinates "+ event.getX() + " "+ event.getY());                   
-        break;
-    case MotionEvent.ACTION_DOWN:
-    	Log.i(TAG, "AD Coordinates "+ event.getX() + " "+ event.getY()); 
-    	sels.addPoint(new Point((int)eventX, (int)eventY));
-    	break;
-    case MotionEvent.ACTION_UP:
-    	Log.i(TAG, "UP Coordinates "+ event.getX() + " "+ event.getY());
-    	sels.addMovingPoint(new Point((int)eventX, (int)eventY));
-      break;
-    default:
-      return false;
-    }
-    // Schedules a repaint.
-    invalidate();
-    return true;
-  }
-} 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float eventX = event.getX();
+		float eventY = event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_MOVE:
+			Log.i(TAG, "MOVE Coordinates " + event.getX() + " " + event.getY());
+			sels.movingPoint(new Point((int) eventX, (int) eventY));
+			break;
+		case MotionEvent.ACTION_POINTER_DOWN:
+			Log.i(TAG, "APD Coordinates " + event.getX() + " " + event.getY());
+			break;
+		case MotionEvent.ACTION_DOWN:
+			Log.i(TAG, "AD Coordinates " + event.getX() + " " + event.getY());
+			sels.addPoint(new Point((int) eventX, (int) eventY));
+			break;
+		case MotionEvent.ACTION_UP:
+			Log.i(TAG, "UP Coordinates " + event.getX() + " " + event.getY());
+			sels.addMovingPoint(new Point((int) eventX, (int) eventY));
+			break;
+		default:
+			return false;
+		}
+		// Schedules a repaint.
+		invalidate();
+		return true;
+	}
+}
